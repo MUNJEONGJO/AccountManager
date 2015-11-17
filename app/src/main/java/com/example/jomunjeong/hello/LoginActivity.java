b61ab82d6ca1e506cb1398532b018a4fb2a09e9b
MUNJEONGJO <dnpsel04@naver.com>
2015-11-17 09:40:04 +0900

    test

--- app/src/main/java/com/example/jomunjeong/hello/LoginActivity.java
+++ app/src/main/java/com/example/jomunjeong/hello/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+//test
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout usernameWrapper;
